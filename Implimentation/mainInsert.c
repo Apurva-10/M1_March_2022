e0fb0902c20c2e19a97e6efc40a60f5c15cee3fc
apurva d <85248357+Apurva-10@users.noreply.github.com>
2022-03-31 14:48:50 +0530

    Update mainInsert.c

--- Implimentation/mainInsert.c
+++ Implimentation/mainInsert.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 #include "arrayUtil.h"
 
-//author: Kollen Gruizenga
+
 //main method code for insertionSort
 int main(){
   int arrayTest[11] = {19, 18, 21, 42, -6, -9, 13, 10, 9, 7, 15};
